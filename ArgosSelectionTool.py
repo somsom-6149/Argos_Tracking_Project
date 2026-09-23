82992a65386b159ab764647749553da0906d4879
somsom-6149 <smstella3462@gmail.com>
2026-09-23 11:25:24 -0400

    Task 5: read one line from the csv

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -17,20 +17,28 @@ the_box = {
     'y_max' : -75.00
 }
 
-# Copy and paste a line of data as the lineString variable value
-lineString = '10154641232,true,2019-05-14 13:37:52.000,-75.49356999999998,34.86216,,0.0,-127.0,4.0167976787E8,5141.0,424,"40",34.86216,34.86216,"0",-75.49356999999998,-75.49356999999998,6,0,3,61.0,381.0,10718.0,2466.0,150,187,2,0,"1",,,"argos-doppler-shift","Pterodroma hasitata","174441","HA09","Satellite tracking of black-capped petrels, 2019"'
-    
+#Create a variable pointing to the data file
+file_name = 'data/raw/Satellite tracking of black-capped petrels 2019-argos.csv'
+
+#Read the contents of the file into a list of lines
+with open(file_name,'r') as f:
+    #Read contents of file into a list
+	line_list = f.readlines()
+
+#Pretend we read one line of data from the file
+lineString = line_list[250]
+
 # Use the split command to parse the items in lineString into a list object
 line_data = lineString.split(',')
   
 # Assign variables to specfic items in the list
 event_id = line_data[0]   # Argos tracking event ID ("event-id")
 timestamp = line_data[2]  # Observation date ("timestamp")
-lat = float(line_data[4])       # Observation latitude  ("location-lat")
+lat = float(line_data[4])        # Observation latitude  ("location-lat")
 lon = float(line_data[3])        # Observation longitude ("location-lon")
-lc  = line_data[14]       # Observation location class ("argos:lc")
-tag_id = line_data[-3]    # Tag identifier ("tag-local-identifier")
-
+lc  = line_data[14]        # Observation location class ("argos:lc")
+tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
+  
 #Evaluate latitude and longitude conditions
 lat_condition = the_box['y_min'] < lat < the_box['y_max']
 lon_condition = the_box['x_min'] < lon < the_box['x_max']
