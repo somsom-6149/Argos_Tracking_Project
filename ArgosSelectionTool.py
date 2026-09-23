df6803baf84565af80160080d346ae05ce9211a7
somsom-6149 <smstella3462@gmail.com>
2026-09-23 10:48:27 -0400

    add code to define geographic box

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -9,3 +9,10 @@
 # Date:   Fall 2026
 #--------------------------------------------------------------
 
+# Create the geographic selection box
+the_box = {
+    'x_min' : 34.00,
+    'y_min' : -76.00,
+    'x_max' : 34.50,
+    'y_max' : -75.00
+}
