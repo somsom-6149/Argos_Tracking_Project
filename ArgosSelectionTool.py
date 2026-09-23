c2780e9ac10fd168cd6e072211e29bfa95a9472a
somsom-6149 <smstella3462@gmail.com>
2026-09-23 11:45:26 -0400

    Task 7: Process lines with while loop

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -21,12 +21,14 @@ the_box = {
 file_name = 'data/raw/Satellite tracking of black-capped petrels 2019-argos.csv'
 
 #Read the contents of the file into a list of lines
-with open(file_name,'r') as f:
-    #Read contents of file into a list
-	line_list = f.readlines()
+f = open(file_name,'r')
+#Read the headerline
+headerLine = f.readline()
+#Read contents of one line
+lineString = f.readline()
 
 #Pretend we read one line of data from the file
-for lineString in line_list[1:]:
+while lineString != "":
 
     # Use the split command to parse the items in lineString into a list object
     line_data = lineString.split(',')
@@ -36,7 +38,8 @@ for lineString in line_list[1:]:
     timestamp = line_data[2]  # Observation date ("timestamp")
     lc  = line_data[14]        # Observation location class ("argos:lc")
     if lc not in ['"1"', '"2"', '"3"']:
-         continue
+        lineString = f.readline()
+        continue
     lat = float(line_data[4])        # Observation latitude  ("location-lat")
     lon = float(line_data[3])        # Observation longitude ("location-lon")
     tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
@@ -50,3 +53,9 @@ for lineString in line_list[1:]:
         print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
     else:
         print(f'Record {event_id}: {tag_id} was NOT IN the box at {timestamp}')
+
+    #update line string
+    lineString = f.readline()
+
+#Close the file
+f.close()
