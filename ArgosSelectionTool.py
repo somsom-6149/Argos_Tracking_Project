c1ba2ae075f3075a3a8571bf765cc43694ce4d75
somsom-6149 <smstella3462@gmail.com>
2026-09-23 10:43:27 -0400

    initial commit of Argos script

---
+++ ArgosSelectionTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ArgosSelectionTool.py
+#
+# Description: Reads in an Argos tracking data file and allows
+#   the user to identify the tracked sitings found within a 
+#   specified bounding box.
+#
+# Author: John Fay (john.fay@duke.edu)
+# Date:   Fall 2026
+#--------------------------------------------------------------
+
